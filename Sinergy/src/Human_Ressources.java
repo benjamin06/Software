4cfa34d247c0cd1a1c2afa8c6f161f4698ca7048
LaLeffe <34269258+LaLeffe@users.noreply.github.com>
2018-03-04 15:50:42 +0100

    Update Human_Ressources.java

--- Sinergy/src/Human_Ressources.java
+++ Sinergy/src/Human_Ressources.java
@@ -4,10 +4,10 @@ public abstract class Human_Ressources{
       public ArrayList<Patient> waiting_queue;
       
       public Human_Ressources(hashcode id, State state){
-						this.id = id;
-						this.state = state;
-						this.waiting_queue = new ArrayList();
-						}
-			}
+	     this.id = id;
+	     this.state = state;
+	     this.waiting_queue = new ArrayList();
+      }
+}
 			
             
